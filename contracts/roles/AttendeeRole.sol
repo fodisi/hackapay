9aa1b879e44af888227f6caec3ab03870b44b723
Francis Odisi <francis.odisi@shiftmarkets.com>
2019-07-16 16:34:13 -0400

    Add roles

---
+++ contracts/roles/AttendeeRole.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.5.0;
+
+import "openzeppelin-solidity/contracts/access/Roles.sol";
+
+contract AttendeeRole {
+  using Roles for Roles.Role;
+
+  event AttendeeAdded(address indexed account);
+  event AttendeeRemoved(address indexed account);
+
+  Roles.Role private _attendees;
+
+  constructor() internal {
+    _addAttendee(msg.sender);
+  }
+
+  modifier onlyAttendee() {
+    require (isAttendee(msg.sender), "AttendeeRole: caller does not have Attendee Role.");
+    _;
+  }
+
+  function isAttendee(address account) public view returns (bool) {
+    return _attendees.has(account);
+  }
+
+    function addAttendee(address account) public onlyAttendee {
+        _addAttendee(account);
+    }
+
+    function renounceAttendee() public {
+        _removeAttendee(msg.sender);
+    }
+
+    function _addAttendee(address account) internal {
+        _attendees.add(account);
+        emit AttendeeAdded(account);
+    }
+
+    function _removeAttendee(address account) internal {
+        _attendees.remove(account);
+        emit AttendeeRemoved(account);
+    }
+}
---
+++ contracts/roles/OrganizerRole.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.5.0;
+
+import "openzeppelin-solidity/contracts/access/Roles.sol";
+
+contract OrganizerRole {
+  using Roles for Roles.Role;
+
+  event OrganizerAdded(address indexed account);
+  event OrganizerRemoved(address indexed account);
+
+  Roles.Role private _organizers;
+
+  constructor() internal {
+    _addOrganizer(msg.sender);
+  }
+
+  modifier onlyOrganizer() {
+    require (isOrganizer(msg.sender), "OrganizerRole: caller does not have Organizer Role.");
+    _;
+  }
+
+  function isOrganizer(address account) public view returns (bool) {
+    return _organizers.has(account);
+  }
+
+    function addOrganizer(address account) public onlyOrganizer {
+        _addOrganizer(account);
+    }
+
+    function renounceOrganizer() public {
+        _removeOrganizer(msg.sender);
+    }
+
+    function _addOrganizer(address account) internal {
+        _organizers.add(account);
+        emit OrganizerAdded(account);
+    }
+
+    function _removeOrganizer(address account) internal {
+        _organizers.remove(account);
+        emit OrganizerRemoved(account);
+    }
+}
