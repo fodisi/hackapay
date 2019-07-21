4a7795fbb9568bb380631d498163d1c55d1a7a30
fodisi <fodisi@users.noreply.github.com>
2019-07-21 17:31:09 -0400

    Updated Copyright to reference source

--- test/roles/Role.behavior.js
+++ test/roles/Role.behavior.js
@@ -1,4 +1,27 @@
-// Based on https://github.com/OpenZeppelin/openzeppelin-solidity/blob/master/test/behaviors/access/roles/PublicRole.behavior.js
+// The MIT License(MIT)
+
+// Copyright(c) 2016 - 2019 zOS Global Limited
+
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files(the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+//   distribute, sublicense, and / or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+
+// The above copyright notice and this permission notice shall be included
+//   in all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+//   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+// Source https://github.com/OpenZeppelin/openzeppelin-solidity/blob/master/test/behaviors/access/roles/PublicRole.behavior.js
 
 const {expectRevert, constants, expectEvent} = require("openzeppelin-test-helpers");
 
@@ -10,21 +33,23 @@ function capitalize(str) {
   return str.replace(/\b\w/g, (l) => l.toUpperCase());
 }
 
-// Tests that a role complies with the standard role interface, that is:
-//  * an onlyRole modifier
-//  * an isRole function
-//  * an addRole function, accessible to role havers
-//  * a renounceRole function
-//  * roleAdded and roleRemoved events
-// Both the modifier and an additional internal remove function are tested through a mock contract that exposes them.
-// This mock contract should be stored in this.contract.
-//
-// @param authorized an account that has the role
-// @param otherAuthorized another account that also has the role
-// @param other an account that doesn't have the role, passed inside an array for ergonomics
-// @param rolename a string with the name of the role
-// @param manager undefined for regular roles, or a manager account for managed roles. In these, only the manager
-// account can create and remove new role bearers.
+/**
+ * @description Tests that a role complies with the standard role interface, that is:
+ *   * an onlyRole modifier
+ *   * an isRole function
+ *   * an addRole function, accessible to role havers
+ *   * a renounceRole function
+ *   * roleAdded and roleRemoved events
+ *  Both the modifier and an additional internal remove function are tested through a mock contract that exposes them.
+ *  This mock contract should be stored in this.contract.
+ * @dev Based on OpenZeppelin test code, with a few improvements and bug fixes regarding expected revert messages.
+ * @param authorized an account that has the role
+ * @param otherAuthorized another account that also has the role
+ * @param other an account that doesn't have the role, passed inside an array for ergonomics
+ * @param rolename a string with the name of the role
+ * @param manager undefined for regular roles, or a manager account for managed roles. In these, only the manager
+ * account can create and remove new role bearers.
+ */
 function shouldBehaveLikeRole(authorized, otherAuthorized, [other], rolename, manager) {
   rolename = capitalize(rolename);
 
