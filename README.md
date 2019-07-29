eb262093e42c7b7506fff4046b3840c6d0b06989
fodisi <fodisi@users.noreply.github.com>
2019-07-29 12:48:54 -0400

    dependencies, configs and readme update

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-###Hackapay
+#Hackapay
+
+###Deploy
+
+#####Main and test nets with infura
+rename credentials-sample.js to credentials.js
+`mv credentials-sample.js credentials.js`
+configure the project id and mnemonic
---
+++ credentials-sample.js
@@ -0,0 +1,8 @@
+export const credentials = {
+  infuraMainnet: {url: "mainnet.infura.io/v3/", projectId: "project-id"},
+  infuraRopsten: {url: "ropsten.infura.io/v3/", projectId: "project-id"},
+  metamaskMnemonic: "twelve words you can find in metamask/settings/reveal seed words blabla",
+  deployerAccount: 0x0,
+};
+
+export default credentials;
