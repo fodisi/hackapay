d877ce3f36065415af051144ceb988109c335ada
fodisi <fodisi@users.noreply.github.com>
2019-07-29 01:07:31 -0400

    Hackathon mock and unit testing

---
+++ contracts/mocks/HackathonMock.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.5.0;
+
+import "../core/Hackathon.sol";
+
+contract HackathonMock is Hackathon {
+    constructor(uint256 _id, bytes32 _name, bytes32 _description) public Hackathon(_id, _name, _description) {
+        //
+    }
+
+    function dummyAllocatePrizesMock() external onlyOrganizer {
+        prizesAllocated = true;
+    }
+
+    function dummyPublishRankMock() external onlyOrganizer {
+        rankPublished = true;
+    }
+
+    function getPrizeAllocationStatus() external view returns (bool) {
+        return prizesAllocated;
+    }
+}
