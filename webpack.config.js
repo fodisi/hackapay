d33288db8297eec6f1cf708faa1385bb669796c6
fodisi <francis2301@gmail.com>
2020-01-30 07:04:14 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: ["@babel/polyfill", "./src/index.js"],
+  devtool: "inline-source-map",
   module: {
     rules: [
       {
